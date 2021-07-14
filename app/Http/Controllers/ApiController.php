07ff56118f0789a470c900e265fa2b6604aabe9b
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2021-07-14 11:56:38 -0300

    apitodo | criando uma tarefa

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,14 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
 
-    public function createTodo()
+    public function createTodo(Request $request)
     {
+        $array = ['error' => ''];
 
+        //validação
+        $rules = [
+            'tittle' => 'required|min:3'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            $array['error'] = $validator->getMessageBag();
+          //$array['error'] = $validator->messages();
+        }
+
+        //fazendo inserção no banco
+        $todo = new Todo();
+        $todo->tittle = $request->input('tittle');
+        $todo->save();
+
+        return $array;
     }
 
     public function readAllTodos()
